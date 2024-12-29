69323c08a76359e6db6c557f0e94173b3d7f5266
Muhammad Usman <musman932@hotmail.com>
2024-12-28 21:38:23 -0700

    Fixed other problems and now it prints in sorted order

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ def main():
     text = read_book(book_path)
     words = num_of_words(text)
     charDict = count_characters(text)
-    print(charDict)
+    sorted_list = chars_dict_to_sorted_list(charDict)
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(words, "words found in the document \n")
+
+    for item in sorted_list:
+        print(f"The '{item['char']}' character was found {item['num']} times")
 
 
 def read_book(book):
@@ -21,9 +27,22 @@ def count_characters(text):
     charCount = {}
     newText = text.lower()
     for character in newText:
-        charCount[character] = charCount.get(character, 0) + 1
+        if character.isalpha():
+            charCount[character] = charCount.get(character, 0) + 1
 
     return charCount
+
+
+def sort_on(dict):
+    return dict["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 
 main()
